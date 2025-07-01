bd531a0cc7888069930adcf4a69ac3009f598683
EBENEZER <Ebenezer.Abate@wsu.edu>
2025-07-01 01:48:17 -0700

    challenge 6- complete added made a new DOM element and added to the intro along with ternary operator for grammatical accuracy./

--- index.js
+++ index.js
@@ -185,11 +185,19 @@ const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
 
+const unfundedGamesCount = GAMES_JSON.filter(game => game.pledged < game.goal).length;
+
+
 
 // create a string that explains the number of unfunded games using the ternary operator
 
+const displaystr= `A total of $${totalRaised.toLocaleString()} has been raised for ${totalGames} ${totalGames>1?'games':'game'}. Currently, ${unfundedGamesCount} ${unfundedGamesCount>1?'games':'game'} remain unfunded.`;
+
 
 // create a new DOM element containing the template string and append it to the description container
+const descriptionElement = document.createElement("p");
+descriptionElement.innerHTML = displaystr;
+descriptionContainer.appendChild(descriptionElement);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
