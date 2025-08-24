e35ffd25a7d6d91904540a6e2e6da0f3dc98a87f
EBENEZER <Ebenezer.Abate@wsu.edu>
2025-08-23 20:23:49 -0700

    removed unnecessary event listener for DOMContentLoaded

--- index.js
+++ index.js
@@ -550,9 +550,7 @@ function showAllGames() {
     // grab the contributions card element
     
     // implement a search function that allows users to search for a game by name
-
  
-document.addEventListener('DOMContentLoaded',init);
  
 
 // let arr=[1,2,3,4,5,6,7,8,9,10];
